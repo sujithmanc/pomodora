baf20a631255715d22d74fd576b2a9cc9ec77c8f
Sujith <sujithmsjs@gmail.com>
2025-12-06 09:06:13 +0530

    Update CorsConfig.java

--- src/main/java/tech/suji/config/CorsConfig.java
+++ src/main/java/tech/suji/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
